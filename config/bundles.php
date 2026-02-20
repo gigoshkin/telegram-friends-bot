388441825834c908ad86156acabbf35c47e22dea
Giorgi Kiknadze <g.kiknadze@self.ge>
2026-02-20 10:36:02 +0400

    feat(): install make bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SergiX44\NutgramBundle\NutgramBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
