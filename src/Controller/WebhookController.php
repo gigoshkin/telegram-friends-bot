fa4ab6c6ed7acb9f27becf60b580fc08d3f27294
Giorgi Kiknadze <g.kiknadze@self.ge>
2026-03-22 21:44:53 +0400

    refactor(WebhookController): remove unused test route and dependencies
    - Removed the `test` route and associated logic to simplify the controller.
    - Dropped the unused `ChatExportParser` dependency for cleaner code.

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\ChatExportParser;
 use SergiX44\Nutgram\Nutgram;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,18 +13,6 @@ final class WebhookController extends AbstractController
     public function index(Nutgram $bot): Response
     {
         $bot->run();
-
-        return new Response();
-    }
-
-    #[Route('/test', name: 'app_test')]
-    public function test(): Response
-    {
-        $parser = new ChatExportParser(
-            $this->getParameter('kernel.project_dir') . '/chat_exports/2_7bc1ea76227da8c9.json'
-        );
-
-        $parser->getMembers();
 
         return new Response();
     }
